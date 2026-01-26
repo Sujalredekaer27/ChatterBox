9da36bae4a7e0afbf21cb34656d885e64c83c633
Sujal Sanjay Redekar <sujalredekar27@gmail.com>
2026-01-26 19:31:38 +0530

    feat: implement advanced error handling and finalized modern UI

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const Chat = require("./models/chat.js");
 
+const ExpressError = require("./ExpressError.js");
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
@@ -20,13 +21,17 @@ main()
     })
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+  await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
 }
 
 //Home Route
-app.get("/chats",async (req,res) => {
-    let chats = await Chat.find();
-    res.render("index.ejs",{chats});
+app.get("/chats",async (req,res,next) => {
+    try {
+        let chats = await Chat.find();
+        res.render("index.ejs",{chats});
+    } catch (err) {
+        next(err);
+    }
 });
 
 //New Route
@@ -35,51 +40,77 @@ app.get("/chats/new",(req,res) => {
 });
 
 //Create Route
-app.post("/chats",(req,res) => {
-    let {from,msg,to} = req.body;
-    let newChat = new Chat({
-        from: from,
-        msg: msg,
-        to: to,
-        created_at: new Date(),
-    });
-    newChat.save()
-        .then((res) => {
-            console.log("new chat is saved to database");
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-    res.redirect("/chats");
+app.post("/chats",(req,res,next) => {
+    try{
+        let {from,msg,to} = req.body;
+        let newChat = new Chat({
+            from: from,
+            msg: msg,
+            to: to,
+            created_at: new Date(),
+        });
+        newChat.save();
+        res.redirect("/chats");
+    } catch(err) {
+        next(err);
+    }
+});
+
+//New - show Route
+app.get("/chats/:id",async(req,res,next) => {
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    if (!chat) {
+        next( new ExpressError (404,"page not found"));
+    }
+    res.render("edit.ejs",{chat});
 });
 
 //Edit Route
-app.get("/chats/:id/edit",async (req,res) => {
-    let {id} = req.params;
-    let chat = await Chat.findById(id);
-    res.render("edit.ejs",{chat});
-})
-
-//Update Route
-app.patch("/chats/:id",async (req,res) => {
-    let {id} = req.params;
-    let {newMsg} = req.body;
-    console.log(newMsg);
-    let updatedChat = await Chat.findByIdAndUpdate(id,{msg:newMsg,created_at:new Date()},{runValidators:true,new:true});
-    res.redirect("/chats");
-    console.log(updatedChat);
+app.get("/chats/:id/edit",async (req,res,next) => {
+    try{
+        let {id} = req.params;
+        let chat = await Chat.findById(id);
+        res.render("edit.ejs",{chat});
+    } catch (err) {
+        next(err);
+    }
 });
 
+//Update Route
+app.patch("/chats/:id",async (req,res,next) => {
+    try {
+        let {id} = req.params;
+        let {newMsg} = req.body;
+        console.log(newMsg);
+        let updatedChat = await Chat.findByIdAndUpdate(id,{msg:newMsg,created_at:new Date()},{runValidators:true,new:true});
+        res.redirect("/chats");
+        console.log(updatedChat);
+    } catch (err) {
+        next(err);
+    }
+});
+
+
 //Delete Route
-app.delete("/chats/:id",async (req,res) => {
-    let {id} = req.params;
-    let deletedChat = await Chat.findByIdAndDelete(id);
-    console.log(deletedChat);
-    res.redirect("/chats");
+app.delete("/chats/:id",async (req,res,next) => {
+    try {
+        let {id} = req.params;
+        let deletedChat = await Chat.findByIdAndDelete(id);
+        console.log(deletedChat);
+        res.redirect("/chats");
+    } catch (err) {
+        next(err);
+    }
 });
 
 app.get("/",(req,res) => {
     res.send("root is working");
+});
+
+app.use((err,req,res,next) => {
+    let {status = 500, message = "some error occurred"} = err;
+    res.status(status).send(message);
 });
 
 app.listen(port,() => {
